8a61caa5088c11148da51942413b4df91aca75db
Nicolas Montredon <nicolas.montredon@gmail.com>
2024-02-28 00:46:45 +0100

    adding nvim config

---
+++ nvim/lua/watchgift/lazy/init.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "nvim-lua/plenary.nvim",
+    },
+--    {
+--        "folke/zen-mode.nvim",
+--        opts = {
+--        -- your configuration comes here
+--        -- or leave it empty to use the default settings
+--        -- refer to the configuration section below
+--        }
+--    },
+    -- "github/copilote.vim",
+    -- "eandrju/cellular-automaton.nvim",
+    -- "laytan/cloak.nvim",
+}
+
